48255c1f07d2af998333ac0b436b4d11684bab4a
bhavesh42 <bajajb@rpi.edu>
2022-07-18 20:25:44 -0400

    History Class
    Fully functional History class is now complete, supporting:
    - Constructor
    - get_history()
    - append_history()
    - export_history()

--- history.py
+++ history.py
@@ -1,7 +1,35 @@
+import pandas as pd
+import os
+
 class History:
-    def __init__(self):#include age as arguement
-        print("History Constructor")
-        #self.age = age Sets to global variable
+    def __init__(self, filename="history"):
+        self.history = pd.DataFrame(columns=["Time", "Exchange", "Path", "Profitability"])
+        self.filename = filename
       
-    def myfunc(self):
-        print("Hello this is a function in History")
+    def get_history(self):
+        return self.history
+    
+    def append_history(self, data):
+        self.history = pd.concat([self.history, data])
+        
+    def export_history(self):
+        os.remove(self.filename + ".xlsx")
+        self.history.to_excel(self.filename + ".xlsx", sheet_name='a', index = False)
+
+# for testing
+
+# obj = History()
+# if(not obj.get_history().empty): print(str(obj.get_history()))
+
+# data = {
+#     "Time": 0.5,
+#     "Exchange": "FTX",
+#     "Path": [['a','b']],
+#     "Profitability": 0.1
+# }
+# df = pd.DataFrame(data)
+# obj.append_history(df)
+# obj.append_history(df)
+# print(str(obj.get_history()))
+# obj.export_history()
+# print("History exported")
