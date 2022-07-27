b511ca5ac8da3495f0bd57bfb52ad5809f03c027
bhavesh42 <bajajb@rpi.edu>
2022-07-27 17:25:38 -0400

    api.py - 10/10 code rating

--- api.py
+++ api.py
@@ -1,27 +1,47 @@
-import json
-import pandas as pd
-import requests
-import re
-from apiOption import *
+"""
+Holds the API class.
+"""
+
+from apiOption import CoinGecko, Coinbase, FTX, Binance, KuCoin, Kraken
 
 class API:
+    """
+    Manages the various APIs used for the program.
+    """
     def __init__(self, selected_option = 0):
+        """
+        Initializes the class using a selected API option.
+        """
         self.selected_option = selected_option
 
-    def get_data(self):
-        apiOption = self.create_api_option()
-        return apiOption.retrieve_data()
-
     def create_api_option(self):
-        apiOption = CoinGecko()
-        if self.selected_option == 1: apiOption = CoinGecko()
-        if self.selected_option == 2: apiOption = Coinbase()
-        if self.selected_option == 3: apiOption = FTX()
-        if self.selected_option == 4: apiOption = Binance()
-        if self.selected_option == 5: apiOption = KuCoin()
-        if self.selected_option == 6: apiOption = Kraken()
+        """
+        Creates and stores a concrete implementation of the abstract APIOption class, using the
+        selected option passed through the initializer.
+        """
+        api_option = CoinGecko()
+        if self.selected_option == 1:
+            api_option = CoinGecko()
+        if self.selected_option == 2:
+            api_option = Coinbase()
+        if self.selected_option == 3:
+            api_option = FTX()
+        if self.selected_option == 4:
+            api_option = Binance()
+        if self.selected_option == 5:
+            api_option = KuCoin()
+        if self.selected_option == 6:
+            api_option = Kraken()
 
-        return apiOption
+        return api_option
+
+    def get_data(self):
+        """
+        Calls the retrieve_data() function on the created concrete implemention of the abstract
+        APIOption class and returns the data returned by that function.
+        """
+        api_option = self.create_api_option()
+        return api_option.retrieve_data()
 
 # driver code
 # obj = API(3)
